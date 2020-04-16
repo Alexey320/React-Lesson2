4d41db9599a3e9e33b47da166a4799f1e14d4606
Your Name <you@example.com>
2020-04-16 19:50:17 +0700

    first component lesson demo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function MyComponent() {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    return <h1 align = "center" > This is The Second React2 Project From Alexey Vorontsov</h1>
+}
+
+class MyOwnComponent extends React.Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {name:'Alexey!!!'}
+    }
+    render() {
+
+         return <div><h1 align = "center" > Здравствуйте мистер {this.state.name}</h1>
+         <h1 align = "center" >This is The Second React2 Project From Alexey Vorontsov</h1></div>
+    }
+}
+
+ReactDOM.render( < MyOwnComponent / > , document.getElementById('root'));
+ReactDOM.render( < MyOwnComponent / > , document.getElementById('root'));
+ReactDOM.render( < MyOwnComponent / > , document.getElementById('root'));
